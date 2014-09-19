0b4f5e36b052e85c96e94dc9e43519c2d82dc8b0
Malachi Griffie <malachi@nexussays.com>
2014-09-19 09:42:26 -0700

    Better formatting for dist/load.d.ts

--- dist/load.d.ts
+++ dist/load.d.ts
@@ -1,7 +1,5 @@
 ﻿declare function load(file: string): load.Promise;
-
 declare function load(fileWithLabel: any): load.Promise;
-
 declare function load(...files: any[]): load.Promise;
 
 declare module load
@@ -21,18 +19,11 @@ declare module load
    function setQuerystring(args: string): void;
 
    function when(dependency: string, callback: () => void, waiting?: (missing: string[]) => void): void;
-
    function when(dependencies: string[], callback: () => void, waiting?: (missing: string[]) => void): void;
-
    function when(dependency: string, file: string, waiting?: (missing: string[]) => void): void;
-
    function when(dependencies: string[], file: string, waiting?: (missing: string[]) => void): void;
-
    function when(dependency: string, files: string[], waiting?: (missing: string[]) => void): void;
-
    function when(dependencies: string[], files: string[], waiting?: (missing: string[]) => void): void;
-
    function when(dependency: string, fileWithLabel: any, waiting?: (missing: string[]) => void): void;
-
    function when(dependencies: string[], fileWithLabel: any, waiting?: (missing: string[]) => void): void;
 }
